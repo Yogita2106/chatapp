7c20b9c838f49b416f4b0a1eda12aab881abd3d9
Yogita Gupta <yogitagupta21062005@gmail.com>
2026-03-31 13:46:50 +0530

    message error

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,14 +1,23 @@
 const Message = require("../models/Message");
 
 exports.getMessages = async (req, res) => {
-  const { userId } = req.params;
+  try {
+    // Frontend se 'sender' aur 'receiver' query params mein aa rahe hain
+    const { sender, receiver } = req.query; 
 
-  const messages = await Message.find({
-    $or: [
-      { sender: req.user.id, receiver: userId },
-      { sender: userId, receiver: req.user.id },
-    ],
-  }).sort({ createdAt: 1 });
+    if (!sender || !receiver) {
+      return res.status(400).json({ message: "Sender and Receiver IDs are required" });
+    }
 
-  res.json(messages);
+    const messages = await Message.find({
+      $or: [
+        { sender: sender, receiver: receiver },
+        { sender: receiver, receiver: sender },
+      ],
+    }).sort({ createdAt: 1 });
+
+    res.json(messages);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching messages" });
+  }
 };
