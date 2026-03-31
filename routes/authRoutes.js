12bf4e0e8e5c5514484084ab01c5afda3107e767
Yogita Gupta <yogitagupta21062005@gmail.com>
2026-03-31 13:37:29 +0530

    user changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { register, login } = require("../controllers/authController");
+// 1. Yahan getUsers ko add karo
+const { register, login, getUsers } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+
+// 2. Ye naya route add karo jo frontend mang raha hai
+router.get("/users", getUsers); 
 
 module.exports = router;
